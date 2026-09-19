a049fe9400d50e754ea9aedba1f746c584025e3a
Ibrahim Mohamed <Ibrahim_20210007@fci.helwan.edu.eg>
2026-09-19 13:00:23 +0300

    d

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer(){
     return <div className="w-full flex flex-wrap items-center justify-between py-[2rem] px-[3rem] text-[#100000] text-[1rem] border-t border-[#EFE6D5]">
 <p>© 2026  All rights reserved.</p>
 <p>hello@example.com</p>
-<p>+201234567891</p>
+<p>+20 10 50508931</p>
 <Link href={"/contact"}>Linkedin</Link>
 {/* <Link href={"/contact"}>CV</Link> */}
     </div>
